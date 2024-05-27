3a7724fd735b220daff6ea38c06ee1701d81b960
binduthotal <bindus170@gmail.com>
2024-05-27 10:57:21 +0530

    gemini commit

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,8 +2,8 @@ import OpenAI from "openai";
 import { OPENAI_API_KEY } from "./constants";
 
 const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
+  // apiKey: OPENAI_API_KEY,
+  // dangerouslyAllowBrowser: true,
 });
 
 export default openai;
